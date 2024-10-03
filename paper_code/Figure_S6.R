4d652ad382b9d3cc283918d087e5400d780f63a4
Robert Wang <85811621+RNA-ROB@users.noreply.github.com>
2024-10-03 19:47:10 -0400

    Create Figure_S6.R

---
+++ paper_code/Figure_S6.R
@@ -0,0 +1,52 @@
+#!/usr/bin/env Rscript
+
+# Author: Robert Wang (Xing Lab)
+# Date: 2024.10.03
+# Supplementary Figure 6
+
+# Minor haplotype expression ratios for genes with heterozygous pathogenic variants in previously diagnosed patients.
+# P values are computed from a haplotype dosage outlier test, which assess whether haplotype expression in a sample 
+# is consistent with those observed in tissue-matched GTEx controls (Methods).
+
+# =====================================================================================================================
+#                                                      LIBRARIES 
+# =====================================================================================================================
+
+# Load required libraries
+suppressMessages(library(dplyr))
+suppressMessages(library(ggplot2))
+suppressMessages(library(tidyr))
+
+# =====================================================================================================================
+#                                                        MAIN
+# =====================================================================================================================
+
+# Establish working directories and file paths
+workdir <- "/mnt/isilon/lin_lab_share/STRIPE"
+outfile <- file.path(workdir, "manuscript/Supplementary_Figures/Figure_S6/Figure_S6.pdf")
+
+# Read in Table_S6.txt and compute haplotype expression ratios for the minor haplotype
+inDF <- read.table(file.path(workdir, "manuscript/Supplementary_Tables/Table_S6/Table_S6.txt"), sep = "\t", 
+    header = TRUE) %>% mutate(minor_class = ifelse(hap1_read_count > hap2_read_count, hap2_variant_class,
+    hap1_variant_class), minor_ratio = ifelse(hap1_read_count > hap2_read_count, hap2_read_count/(hap1_read_count +
+    hap2_read_count), hap1_read_count/(hap1_read_count + hap2_read_count)))
+classLevels <- aggregate(inDF$minor_ratio, list(inDF$minor_class), FUN = mean) %>% arrange(-x) %>% pull(Group.1)
+inDF$minor_class <- factor(as.character(inDF$minor_class), levels = classLevels)
+
+# Cap off maximum -log10 p-value to 4
+inDF$log_pval <- -log10(inDF$p_value)
+inDF$log_pval[inDF$log_pval > 4] <- 4
+
+# Plot minor haplotype expression ratios for different classes of pathogenic variants
+palette <- setNames(c("#F7A63D", "#63BA96", "#4C78B9", "#88CCEE", "#CE90BE", "#954492", "#E83578", "#223671"), 
+    c("Missense", "Synonymous", "Splice donor", "Splice acceptor", "Inframe deletion", "Frameshift duplication", 
+    "Nonsense", "Structural deletion"))
+p <- ggplot(inDF, aes(x = minor_ratio * 100, y = minor_class, size = log_pval, color = minor_class)) + geom_point(shape = 4) +
+    theme_classic() + xlab("Minor haplotype expression ratio (%)") + theme(panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(), panel.background = element_blank(), axis.text = element_text(color = "black", size = 6), 
+    axis.title.x = element_text(color = "black", size = 7), axis.title.y = element_blank(), axis.ticks = element_line(color = "black", linewidth = 0.25), 
+    legend.title = element_text(color = "black", size = 7), legend.text = element_text(color = "black", size = 6)) +
+    guides(color = "none", size = guide_legend("-log10[p-value]")) + scale_size_continuous(breaks = seq(4), limits = c(0, 4),
+    range = c(0.5, 2)) + coord_cartesian(xlim = c(0, 50)) + scale_color_manual(values = palette)
+
+ggsave(outfile, plot = p, width = 4, height = 1.75)
