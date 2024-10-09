6797cf2a02cbcaac946e0c5f1b9039800fdb4b46
Robert Wang <85811621+RNA-ROB@users.noreply.github.com>
2024-10-09 01:48:55 -0400

    Update Table_S9.py

--- paper_code/Table_S9.py
+++ paper_code/Table_S9.py
@@ -33,9 +33,10 @@ outDF = pd.DataFrame(columns = ['patient_id', 'gene_panel', 'gene_name', 'varian
 # Iterate over variant calling results (STRIPE) for each undiagnosed patient
 targets = list(pd.read_csv(workdir + '/CDG/references/target_genes.bed', sep = '\t', header = None)[4])
 sampleDF = pd.read_csv(workdir + '/CDG/samples.txt', sep = '\t', header = 0)
+cohort = list(sampleDF[sampleDF['Provider'] != 'Lan Lin']['ID'])
 undiagnosed = list(sampleDF[(sampleDF['Status'] == 'Undiagnosed') & (sampleDF['Provider'] != 'Lan Lin')]['ID'])
 
-for patient in undiagnosed:
+for patient in cohort:
     for target in targets:
         infile = workdir + '/CDG/' + patient + '/RNA/stripe/target_genes/' + target + '/variant_calling/merged_variants.tsv'
         if os.path.isfile(infile):
@@ -49,12 +50,18 @@ for patient in undiagnosed:
                 outDF = pd.concat([outDF, pd.DataFrame([[patient, 'CDG-466', target, item[0] + ':' + str(item[1]) + ' ' + item[2] + '>' + item[3]] + list(item[4:]) 
                     for item in inDF.values], columns = outDF.columns)])
 
-# Only keep variants that appear in no more than one undiagnosed individual and meet the following criteria:
+# Drop 'gnomad_nhomalt_grpmax_joint' and 'homopolymer' columns from outDF (these columns are now uninformative)
+outDF = outDF.drop(['gnomad_nhomalt_grpmax_joint', 'homopolymer'], axis = 1)
+
+# Only keep variants that appear in no more than one individual and meet the following criteria:
 #   * Proportion of reads with variant found at end < 0.5
 #   * Proportion of reads skipping variant locus < 0.5
 variant_counts = outDF['variant_grch38'].value_counts()
 outDF = outDF[outDF['variant_grch38'].isin(set(variant_counts[variant_counts == 1].index))]
 outDF = outDF[(outDF['end_distance_pct'] < 0.5) & (outDF['skipping_frequency'] < 0.5)]
+
+# Filter outDF for variant calls in undiagnosed cases
+outDF = outDF[outDF['patient_id'].isin(set(undiagnosed))]
 
 # Save outDF to outfile
 outDF.to_csv(outfile, sep = '\t', index = False)
