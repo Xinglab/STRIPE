4b2e64bb1037f86a5a0795b957d7b9bfaa474060
Robert Wang <85811621+RNA-ROB@users.noreply.github.com>
2025-10-08 19:32:30 -0400

    Update Figure_S12a.sh

--- paper_code/Figure_S12a.sh
+++ paper_code/Figure_S12a.sh
@@ -4,9 +4,9 @@
 # Date: 2025.03.03
 # Supplementary Figure 12a
 
-# (a) Sashimi plot visualization of haplotype-resolved PIGN splicing patterns in individual CDG-P05. Haplotype 2 carries 
-# a synonymous variant (NM_176787.5:c.963G>A, p.Gln321=) on exon 11 that maps to the -1 position of the intron 11 donor 
-# splice site. 
+# (a) Sashimi plot visualization of haplotype-resolved splicing patterns around PIGN exons 9 to 12 in individual CDG-P05. 
+# Haplotype 2 carries a synonymous variant (NM_176787.5:c.963G>A, p.Gln321=) on exon 11 that maps to the -1 position of 
+# the intron 11 donor splice site. 
 
 # =====================================================================================================================
 #                                                        MAIN
@@ -31,7 +31,7 @@ echo -e "Haplotype 1\t$WORKDIR/manuscript/Revisions/20250228/Supplementary_Table
 echo -e "Haplotype 2\t$WORKDIR/manuscript/Revisions/20250228/Supplementary_Tables/Table_S6/$SAMPID/PIGN/hap2_reads.bam\t2" \
     >> "$WORKDIR/manuscript/Revisions/20250228/Supplementary_Figures/Figure_S12/tmp/input_bam.txt"
 
-# Use ggsashimi.py to visualize haplotype-resolved PIGN splicing patterns in individual CDG-P05
+# Use ggsashimi.py to visualize haplotype-resolved splicing patterns around PIGN exons 9 to 12 in individual CDG-P05
 # Only visualize splice junctions supported by at least 25 reads
 (singularity run -B "$WORKDIR" docker://guigolab/ggsashimi:v1.1.5 -b "$WORKDIR/manuscript/Revisions/20250228/Supplementary_Figures/Figure_S12/tmp/input_bam.txt" \
     -c "chr18:62140420-62147101" -o "$WORKDIR/manuscript/Revisions/20250228/Supplementary_Figures/Figure_S12/Figure_S12a" -M 25 \
